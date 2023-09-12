d69f847d6d7042a26a6af5b188f35adb3ddc8d64
David Kuffour Koranteng <davidkoranteng350@gmail.com>
2023-09-12 13:50:07 +0100

    product api

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,102 +1,204 @@
-import React from 'react';
-import { useCart } from './CartContext';
-import { Card, Button, Container, Col, Row } from 'react-bootstrap';
+// import React from 'react';
+// import { useCart } from './CartContext';
+// import { Card, Button, Container, Col, Row } from 'react-bootstrap';
+// import { images } from './images';
+
+// const Products = () => {
+//   const { cart, setCart } = useCart();
+
+//   const addToCart = (product) => {
+//     setCart([...cart, product]);
+//   };
+
+//   const imageStyle = {
+//     width: '260px', 
+//     height: '200px',
+//   };
+
+//   return (
+//     <Container>
+//       <Row>
+//         <h1>Get Some Of Our Products</h1>
+//         <Col md="3" style={{ margin: '1rem' }}>
+//           <Card>
+//             <Card.Img variant="top" src={images.top1} style={imageStyle} />
+//             <Card.Body>
+//               <Card.Title>Kaba And Slit</Card.Title>
+//               <Card.Text>GHC 50</Card.Text>
+//               <Button
+//                 variant="primary"
+//                 onClick={() => addToCart({ name: 'Kaba And Slit', price: 50 })}
+//               >
+//                 Add to Cart
+//               </Button>
+//             </Card.Body>
+//           </Card>
+//         </Col>
+//         <Col md="3" style={{ margin: '1rem' }}>
+//           <Card>
+//             <Card.Img variant="top" src={images.top2} style={imageStyle} />
+//             <Card.Body>
+//               <Card.Title>Straight Dress</Card.Title>
+//               <Card.Text>GHC 70</Card.Text>
+//               <Button
+//                 variant="primary"
+//                 onClick={() => addToCart({ name: 'Straight Dress', price: 70 })}
+//               >
+//                 Add to Cart
+//               </Button>
+//             </Card.Body>
+//           </Card>
+//         </Col>
+//         <Col md="3" style={{ margin: '1rem' }}>
+//           <Card>
+//             <Card.Img variant="top" src={images.top2} style={imageStyle} />
+//             <Card.Body>
+//               <Card.Title>Straight Dress</Card.Title>
+//               <Card.Text>GHC 70</Card.Text>
+//               <Button
+//                 variant="primary"
+//                 onClick={() => addToCart({ name: 'Straight Dress', price: 70 })}
+//               >
+//                 Add to Cart
+//               </Button>
+//             </Card.Body>
+//           </Card>
+//         </Col>
+//          <Col md="3" style={{ margin: '1rem' }}>
+//           <Card>
+//             <Card.Img variant="top" src={images.top2} style={imageStyle} />
+//             <Card.Body>
+//               <Card.Title>Straight Dress</Card.Title>
+//               <Card.Text>GHC 70</Card.Text>
+//               <Button
+//                 variant="primary"
+//                 onClick={() => addToCart({ name: 'Straight Dress', price: 70 })}
+//               >
+//                 Add to Cart
+//               </Button>
+//             </Card.Body>
+//           </Card>
+//         </Col>
+//          <Col md="3" style={{ margin: '1rem' }}>
+//           <Card>
+//             <Card.Img variant="top" src={images.top2} style={imageStyle} />
+//             <Card.Body>
+//               <Card.Title>Straight Dress</Card.Title>
+//               <Card.Text>GHC 70</Card.Text>
+//               <Button
+//                 variant="primary"
+//                 onClick={() => addToCart({ name: 'Straight Dress', price: 70 })}
+//               >
+//                 Add to Cart
+//               </Button>
+//             </Card.Body>
+//           </Card>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// };
+
+// export default Products;
+
+
+
+// import React from 'react'
+// import { Card, Button } from 'react-bootstrap';
+// import { images } from './images';
+// import { useCart } from './CartContext';
+// import "../styles.css";
+
+// const Products = () => {
+
+//   const { cart, setCart } = useCart();
+
+//    const addToCart = (product) => {
+//      setCart([...cart, product]);
+//    };
+
+//   return (
+//     <>
+//     <h2 className='producthead'>Products</h2>
+//     <div className='productcontainer'>
+//       <Card className='card'>
+//              <Card.Img variant="top" src={images.top1}  />
+//              <Card.Body>
+//                <Card.Title>Kaba And Slit</Card.Title>
+//                <Card.Text>GHC 50</Card.Text>
+//                <Button
+//                  variant="primary"
+//                  onClick={() => addToCart({ name: 'Kaba And Slit', price: 50 })}
+//               >
+//                  Add to Cart
+//                </Button>
+//              </Card.Body>
+//            </Card>
+//     </div>
+      
+//     </>
+//   )
+// }
+
+// export default Products;
+
+import {useEffect,useState} from 'react'
+import axios from 'axios';
+import { Card, Button } from 'react-bootstrap';
 import { images } from './images';
+import { useCart } from './CartContext';
+import "../styles.css";
+
+const productAPI = "http://localhost:4000/wears";
+
 
 const Products = () => {
+   const [things, setThings] = useState([]);
+
+  useEffect(() => {
+    const getproducts = async() => {
+      const resposnse = await axios.get(productAPI);
+      // console.log(resposnse);
+      setThings(resposnse.data)
+    }
+    getproducts();
+  },[]);
+
   const { cart, setCart } = useCart();
 
-  const addToCart = (product) => {
-    setCart([...cart, product]);
-  };
-
-  const imageStyle = {
-    width: '260px', 
-    height: '200px',
-  };
+   const addToCart = (product) => {
+     setCart([...cart, product]);
+   };
 
   return (
-    <Container>
-      <Row>
-        <h1>Get Some Of Our Products</h1>
-        <Col md="3" style={{ margin: '1rem' }}>
-          <Card>
-            <Card.Img variant="top" src={images.top1} style={imageStyle} />
-            <Card.Body>
-              <Card.Title>Kaba And Slit</Card.Title>
-              <Card.Text>GHC 50</Card.Text>
-              <Button
-                variant="primary"
-                onClick={() => addToCart({ name: 'Kaba And Slit', price: 50 })}
+    <>
+    <h2 className='producthead'>Products</h2>
+    <div className='productcontainer'>
+      {things.map((dress) => {
+        return (
+          <>
+          <Card className='card'>
+             <Card.Img variant="top" src={images.top1}  />
+             <Card.Body>
+               <Card.Title>{dress.Title}</Card.Title>
+               <Card.Text>{dress.price}</Card.Text>
+               <Button
+                 variant="primary"
+                 onClick={() => 
+                  addToCart({ name: `{dress.Title}`, price: `{dress.price}` })}
               >
-                Add to Cart
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md="3" style={{ margin: '1rem' }}>
-          <Card>
-            <Card.Img variant="top" src={images.top2} style={imageStyle} />
-            <Card.Body>
-              <Card.Title>Straight Dress</Card.Title>
-              <Card.Text>GHC 70</Card.Text>
-              <Button
-                variant="primary"
-                onClick={() => addToCart({ name: 'Straight Dress', price: 70 })}
-              >
-                Add to Cart
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col md="3" style={{ margin: '1rem' }}>
-          <Card>
-            <Card.Img variant="top" src={images.top2} style={imageStyle} />
-            <Card.Body>
-              <Card.Title>Straight Dress</Card.Title>
-              <Card.Text>GHC 70</Card.Text>
-              <Button
-                variant="primary"
-                onClick={() => addToCart({ name: 'Straight Dress', price: 70 })}
-              >
-                Add to Cart
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-         <Col md="3" style={{ margin: '1rem' }}>
-          <Card>
-            <Card.Img variant="top" src={images.top2} style={imageStyle} />
-            <Card.Body>
-              <Card.Title>Straight Dress</Card.Title>
-              <Card.Text>GHC 70</Card.Text>
-              <Button
-                variant="primary"
-                onClick={() => addToCart({ name: 'Straight Dress', price: 70 })}
-              >
-                Add to Cart
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-         <Col md="3" style={{ margin: '1rem' }}>
-          <Card>
-            <Card.Img variant="top" src={images.top2} style={imageStyle} />
-            <Card.Body>
-              <Card.Title>Straight Dress</Card.Title>
-              <Card.Text>GHC 70</Card.Text>
-              <Button
-                variant="primary"
-                onClick={() => addToCart({ name: 'Straight Dress', price: 70 })}
-              >
-                Add to Cart
-              </Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+                 Add to Cart
+               </Button>
+             </Card.Body>
+           </Card>
+          </>
+        );
+      })}
+      
+    </div>
+      
+    </>
+  )
+}
 
 export default Products;
---
+++ src/components/productdetails.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+
+
+const Productdetails = () => {
+    let p =[1,2,3,4,5,6]
+    let one = p.filter(eachItem=>eachItem==1 )
+    alert(one)
+
+    // USE GET PARAMS TO GET PRODUCT ID
+    //MAKE A GET/FETCH -- THIS GIVES U AN ARRAY OF ALL PROJECT
+    //ALLDATA.FILTER let one = p.filter(eachItem=>eachItem==1 )
+    useEffect(()=> {
+
+    })
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Productdetails
